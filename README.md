6a786fde1f0e89f4b75e226889b045d480ea26dd
FerMarLey <FerMarcelin>
2024-12-16 17:50:52 -0600

    commiting app with new graph changes, notebook session with plotly testing, csv file in order to create the desired graphs and requirements for packages that will be used for render

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TT-Sprint7-FinalProject
+
+
+This project summarizes the acquired knowledge from Sprint 7 - Software Tools. 
+
+In here we are able to deploy a webb app by connecting our github account which contains a repository with a small project that using streamlit and plotly is capable of showing different types of graphs from an specific dataframe
---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv(r'C:/Users/fmarc/OneDrive/Escritorio/TripleTenBootcampDataScience/TTProjects/Sprint7/TT-Sprint7-FinalProject/vehicles_us.csv') # leer los datos
+
+st.header('TT - Proyecto S7 - Car Data')
+
+hist_button = st.button('Construir histograma')
+disp_button = st.button('Contruir un gráfico de dispersión')
+
+if hist_button: # al hacer clic en el botón
+            # escribir un mensaje
+            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+            # crear un histograma
+            fig = px.histogram(car_data, x="odometer")
+        
+            # mostrar un gráfico Plotly interactivo
+            st.plotly_chart(fig, use_container_width=True)
+
+if disp_button: # al hacer clic en el botón
+            # escribir un mensaje
+            st.write('Creación de un grafico de dispersión a para el conjunto de datos de anuncios de venta de coches')
+            
+            # crear un histograma
+            fig = px.scatter(car_data, x="odometer", y="price")
+        
+            # mostrar un gráfico Plotly interactivo
+            st.plotly_chart(fig, use_container_width=True)
